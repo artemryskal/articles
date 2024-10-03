bcc14e758e124be9c98350d9cbafe29ed05644b0
artemryskal <artemryskal01@gmail.com>
2024-10-04 02:08:27 +0300

    Add header design, prettier, tailwind, update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@400;500;700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+        },
+      ],
+    },
+  },
+
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxt/image'],
+
+  image: {
+    dir: 'assets',
+  },
+
+  compatibilityDate: '2024-10-04',
 })
